9ad6863ebad5f44b12dc083c5a6ba3b505406b9a
LuisEnriqueJT <luisenrique.jt@outlook.com>
2022-02-01 13:06:20 -0600

    Se agrega autoloader.php y se carga al index.php

--- 01-POO/index.php
+++ 01-POO/index.php
@@ -1,17 +1,3 @@
 <?php
-	include_once 'Futbolista.php';
-
-	$pp14 = new Futbolista('Pablo','Parra','Parrita','Club puebla','Medio',14);
-	$pp14->amonestar();
-	$pp14->anotarGol();
-	$pp14->anotarGol();
-	//var_dump($pp14);
-	echo $pp14->imprimirDatos($pp14);
-
-	$as1 = new Portero('Antony','Silva','','Club puebla','Portero',1);
-	$as1->amonestar();
-	$as1->amonestar();
-	//var_dump($as1);
-
-	echo $as1->imprimirDatos($as1);
+	require_once 'autoloader.php';
 ?>
