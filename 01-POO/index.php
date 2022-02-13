22f71c8321c222cfcd48780ff8a70c54dd260393
LuisEnriqueJT <luisenrique.jt@outlook.com>
2022-02-12 20:12:13 -0600

    Se instancian objetos futbolista en el index

--- 01-POO/index.php
+++ 01-POO/index.php
@@ -1,3 +1,23 @@
 <?php
 	require_once 'autoloader.php';
+
+	//instancia de la clase Futbolista
+
+	$lj11 = new Futbolista();
+	$lj11->setNombre("Luis");
+	$lj11->setApellido("Jimenez");
+	$lj11->setEquipo("Bengala");
+	$lj11->setPosicion("Medio");
+	$lj11->setDorsal(11);
+	$lj11->setApodo("Cepillo");
+	echo $lj11->imprimirDatos($lj11);
+
+	$er1 = new Portero();
+	$er1->setNombre("Edson");
+	$er1->setApellido("Rivas");
+	$er1->setEquipo("Bengala");
+	$er1->setApodo("Maldito Carnicero");
+	$er1->setPosicion("Portero");
+	$er1->setDorsal(1);
+	echo $er1->imprimirDatos($er1);
 ?>
